70bfb10b38de4a7ed16a85a81de6ec8e6a144894
Brendan O'Connor <brenocon@gmail.com>
2013-10-11 13:51:13 -0400

    build script update

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 cd $(dirname $0)
 set -eux
-(cd bin && jar cf ../myutil.jar .)
-(cd src && jar uf ../myutil.jar .)
-jar uf myutil.jar README.md
+rm -rf _build
+mkdir -p _build
 
+CP="$(print -l lib/**/*.jar | grep -v 'old/' | tr '\n' :)"
+javac -cp "$CP" -d _build $(print -l src/**/*.java | 
+  grep -v 'tests/' | 
+  grep -Pv 'src/(RunSS|GLM).java'
+)
+
+(cd _build && jar cf ../myutil.jar .)
+(cd src && jar uf ../myutil.jar .)
+ls -l myutil.jar
