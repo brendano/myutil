288281cb996d3253d5d052f31d3c3222f2877649
Brendan O'Connor <brenocon@gmail.com>
2015-01-30 14:28:47 -0500

    build tweaks

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ rm -rf _build
 mkdir -p _build
 
 CP="$(print -l lib/**/*.jar | grep -v 'old/' | tr '\n' :)"
-javac -cp "$CP" -d _build $(print -l src/**/*.java | 
+javac -cp "$CP" -d _build $(print -l src/*/**/*.java | 
   grep -v 'tests/' | 
   grep -Pv 'src/(RunSS|GLM).java'
 )
 
 (cd _build && jar cf ../myutil.jar .)
-(cd src && jar uf ../myutil.jar .)
+# (cd src && jar uf ../myutil.jar .)
 ls -l myutil.jar
